bfac7f31bbdc172ea40731a279d84554e00ed2b9
Davi Medeiros <davi@pop-os.localdomain>
2021-04-20 09:23:34 -0300

    alo

--- mongoDb.js
+++ mongoDb.js
@@ -27,6 +27,61 @@ function mongodb(host) {
                     });
                 });
             })
+        },
+        delete: function (database, collection, options, query) {
+            return new Promise((resolve, reject) => {
+                setTimeout(async ()=>{reject("Timeout")}, 5000)
+                MongoClient.connect(host, options, function (err, db) {
+                    if (err) reject(err);
+                    var dbo = db.db(database);
+                    dbo.collection(collection).deleteMany(query, function(err, result) {
+                        if (err) reject(err);
+                        resolve(result);
+                        db.close();
+                    });
+                });
+            })
+        },
+        update: function (database, collection, options, query, newdata) {
+            return new Promise((resolve, reject) => {
+                setTimeout(async ()=>{reject("Timeout")}, 5000)
+                MongoClient.connect(host, options, function (err, db) {
+                    if (err) reject(err);
+                    var dbo = db.db(database);
+                    dbo.collection(collection).updateOne(query, newdata, function(err, result) {
+                        if (err) reject(err);
+                        resolve(result);
+                        db.close();
+                    });
+                });
+            })
+        },
+        query: function (database, collection, options, query, limit) {
+            return new Promise((resolve, reject) => {
+                setTimeout(async ()=>{reject("Timeout")}, 5000)
+                MongoClient.connect(host, options, function (err, db) {
+                    if (err) reject(err);
+                    var dbo = db.db(database);
+                    dbo.collection(collection).find(query).limit(limit).toArray(function (err, result) {
+                        if (err) reject(err);
+                        resolve(result);
+                        db.close();
+                    });
+                });
+            })
+        },
+        join: function (database, collection, options, data) {
+            return new Promise((resolve, reject) => {
+                MongoClient.connect(host, options, function (err, db) {
+                    if (err) reject(err);
+                    var dbo = db.db(database);
+                    dbo.collection(collection).aggregate(data).toArray(function(err, res) {
+                        if (err) reject(err);
+                        resolve(res);
+                        db.close();
+                    });
+                });
+            })
         }
     }
 }
